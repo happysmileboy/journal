91110fbffe84b1ef092b48bc2a05eb3088e27d9e
happysmileboy <t35114105@gmail.com>
2018-12-20 02:47:00 +0900

    hwo

--- core/getdata/S_district.py
+++ core/getdata/S_district.py
@@ -18,16 +18,21 @@ print(keylist)
 
 
 district_url = "http://kweather.co.kr/air/data/area_realtime/MAP_"
-f = open('../kdata_s.csv', 'w', encoding='utf-8')
+f = open('../../static/kdata_s.csv', 'w', encoding='utf-8')
 wr = csv.writer(f)
+i = 0
+
+wr.writerow(['gu','D_name', 'pm10', 'pm25', 'color', 'lat', 'lon'])
 
 for x in keylist:
     with urllib.request.urlopen(district_url + x + ".json") as response:
         dc = json.loads(response.read().decode("utf-8"))
+        gu = dc[list(dc.keys())[0]][0].split("#")[1]
         D_key = list(dc.keys())
         for y in D_key:
             for z in range(1, len(dc[y])-1):
                 pm10 = dc[y][z].split("#")[7]
+                pm25 = dc[y][z].split("#")[9]
                 D_name = dc[y][z].split("#")[1]
                 D_lat = dc[y][z].split("#")[4]
                 D_lon = dc[y][z].split("#")[5]
@@ -43,5 +48,7 @@ for x in keylist:
                     color = 'rgb(255,0,0)'
                 else:
                     color = 'rgb(0,0,0)'
-                wr.writerow([D_name, pm10, color, D_lat, D_lon])
+                if i % 40 == 0:
+                    wr.writerow([gu, D_name, pm10, pm25, color, D_lat, D_lon])
+                i += 1
 f.close()
